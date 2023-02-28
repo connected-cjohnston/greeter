1d9ac6fbe3adee38675137ee598722b5922c7e8b
Chris Johnston <cjohnston@connected.io>
2023-02-28 10:13:53 -0500

    refactors code to remove duplicate time in conditionals

--- Ruby/app/greeter.rb
+++ Ruby/app/greeter.rb
@@ -3,12 +3,12 @@ class Greeter
   def greet(name, time = nil)
     formatted_name = name.strip.capitalize
 
-    if time && time >= Time.parse('6am') && time <= Time.parse('12pm')
-      return "Good morning #{formatted_name}"
-    elsif time && time >= Time.parse('6pm') && time <= Time.parse('10pm')
-      return "Good evening #{formatted_name}"
-    end
+    return "Hello #{formatted_name}" unless time
 
-    "Hello #{formatted_name}"
+    if time >= Time.parse('6am') && time <= Time.parse('12pm')
+      "Good morning #{formatted_name}"
+    elsif time >= Time.parse('6pm') && time <= Time.parse('10pm')
+      "Good evening #{formatted_name}"
+    end
   end
 end
