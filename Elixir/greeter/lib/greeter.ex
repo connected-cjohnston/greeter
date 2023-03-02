4d5acd3f355cbc8b8021f56e308272aaa6523570
Chris Johnston <cjohnston@connected.io>
2023-03-02 11:03:17 -0500

    Elixir: now using string interpolation

--- Elixir/greeter/lib/greeter.ex
+++ Elixir/greeter/lib/greeter.ex
@@ -5,18 +5,18 @@ defmodule Greeter do
   end
 
   def greet(name) do
-    "Hello " <> formattedName(name)
+    "Hello #{formattedName(name)}"
   end
 
   def greet(name, time) when time.hour >= 6 and time.hour <= 12 do
-    "Good morning " <> formattedName(name)
+    "Good morning #{formattedName(name)}"
   end
 
   def greet(name, time) when time.hour >= 18 and time.hour <= 21 do
-    "Good evening " <> formattedName(name)
+    "Good evening #{formattedName(name)}"
   end
 
   def greet(name, time) when time.hour >= 22 or time.hour <= 5 do
-    "Good night " <> formattedName(name)
+    "Good night #{formattedName(name)}"
   end
 end
